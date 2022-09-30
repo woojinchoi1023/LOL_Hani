f966f90402161fc28e23d8a58c527dfae227338f
woojin <woojinchoi1023@gmail.com>
2022-09-30 16:52:57 +0900

    fix better

--- pages/FollowPage.js
+++ pages/FollowPage.js
@@ -22,13 +22,16 @@ export default function FollowPage({ route }) {
   const [followNames, setFollowNames] = useState([]);
   const [followIds, setFollowIds] = useState([]);
   const [pageLoadReady, setPageLoadReady] = useState(false);
-  
+  const [tempNickname, setTempNickname] = useState("");
+  const [tier, setTier] = useState("");
+  const [rank, setRank] = useState("");
+  const [tierData, setTierData] = useState([]);
 
-  let riotApiKey = "RGAPI-171b6255-e638-491b-83ce-393807180bad";
+  let riotApiKey = route.params.riotApiKey
 
   let accountInfo =
     "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
-    nickname +
+    tempNickname +
     "?api_key=" +
     riotApiKey;
   let matchID =
@@ -40,67 +43,166 @@ export default function FollowPage({ route }) {
     "&start=0&count=3";
 
   const loadFollowList = () => {
-    firebase_db.ref("users/" + userId).once("value").then((snapshot) => {
+    firebase_db
+      .ref("users/" + userId)
+      .once("value")
+      .then((snapshot) => {
         let fav = snapshot.val();
         if (fav) {
-        setFollowNames(Object.keys(fav));
-        setFollowIds(Object.values(fav));
+          setFollowNames(Object.keys(fav));
+          setFollowIds(Object.values(fav));
         } else {
-            console.log('비었습니다')
+          console.log("비었습니다");
         }
-        
       });
-  }
+  };
 
-  
   const search = () => {
-    axios.get(accountInfo).catch(error => {alert('없는 소환사명입니다')}).then((response) => {
-        setBigData(response.data);
+    axios
+      .get(accountInfo)
+      .catch((error) => {
+        alert("없는 소환사명입니다");
+      })
+      .then((response) => {
+        if (response) {
+          setBigData(response.data);
+          let idCode = response.data.id;
+          setNickName(tempNickname);
+          loadTier(idCode);
+          alert("검색 완료");
+        }
       });
-    alert('검색 완료')
-  }
+  };
+
+  const loadTier = async (idCode) => {
+    let tierUrl =
+      "https://kr.api.riotgames.com" +
+      "/lol/league/v4/entries/by-summoner/" +
+      idCode +
+      "?api_key=" +
+      riotApiKey;
+    try {
+      let userInfo = await axios.get(tierUrl);
+      setTier(userInfo.data[0]["tier"]);
+      setTierData(userInfo.data);
+      console.log(tierUrl)
+    } catch (err) {
+      console.log("티어로딩 실패");
+    }
+  };
 
   const addFollow = () => {
-    if (followNames.length>4) {alert('최대 5명까지 팔로우 할 수 있습니다.')} else {
-        firebase_db.ref("users/" + userId + "/" + nickname).set(bigData.puuid);
-        alert('팔로우 성공')
+    if (followNames.length > 4) {
+      alert("최대 5명까지 팔로우 할 수 있습니다.");
+    } else {
+      firebase_db.ref("users/" + userId + "/" + nickname).set(bigData.puuid);
+      alert("팔로우 성공");
     }
-        
-        loadFollowList()
-  }
 
+    loadFollowList();
+  };
 
   useEffect(() => {
     loadFollowList();
-    console.log('페이지 로딩 완료');
+    console.log("페이지 로딩 완료");
     setPageLoadReady(true);
-    
   }, []);
 
   return (
     <ScrollView style={styles.container}>
-        <View >
-            <Text style={styles.desc}> 닉네임을 적어주세요 (공백없이) </Text>
-            <TextInput style={styles.input} onChangeText={(text) => setNickName(text)}>
-            </TextInput>
-            <TouchableOpacity style={styles.searchButton} onPress={()=>{search();}}>
-                <Text>검색!</Text>
-            </TouchableOpacity>
-        </View>
-        <View>
-            <Text style={styles.result}> {nickname} 님의 정보입니다 : </Text>
-            <Text style={{ fontSize: 30, color: "red", fontWeight: "bold" }}>
-            레벨: {bigData.summonerLevel}
-          </Text>
-            <TouchableOpacity style={styles.searchButton} onPress={()=>{addFollow();}}>
-                <Text>팔로우하기</Text>
-            </TouchableOpacity>
-        </View>
-        <View>
-            {followNames.map((content,i)=>{
-                return (<Follow content={content} key={i} ID={userId} followNames={followNames} setFollowNames={setFollowNames}/>)
-            })}
-        </View>
+      <View>
+        <Text style={styles.desc}> 닉네임을 적어주세요 (공백없이) </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setTempNickname(text)}
+        ></TextInput>
+        <TouchableOpacity
+          style={styles.searchButton}
+          onPress={() => {
+            search();
+          }}
+        >
+          <Text>검색!</Text>
+        </TouchableOpacity>
+      </View>
+        <View style={{marginVertical:10}}>
+          <View>
+            <Text style={styles.result}>{nickname} 님의 정보입니다 : </Text>
+          </View>
+        
+        {/* <Text style={{ fontSize: 30, color: "red", fontWeight: "bold" }}>
+          레벨: {bigData.summonerLevel}
+        </Text> */}
+        {tierData.map((content, i) => {
+          if (content.queueType === "RANKED_FLEX_SR") {
+            return (
+              <Text
+                key={i}
+                style={{ fontSize: 20, color: "red", fontWeight: "bold" }}
+              >
+                팀랭: {content.tier} {content.rank} 승: {content.wins} 패:{" "}
+                {content.losses}
+              </Text>
+            );
+          } else if (content.queueType === "RANKED_SOLO_5x5"){
+            return (
+              <Text
+                key={i}
+                style={{ fontSize: 20, color: "red", fontWeight: "bold" }}
+              >
+                솔랭: {content.tier} {content.rank} 승: {content.wins} 패:{" "}
+                {content.losses}
+              </Text>
+            );
+          } else if(content.queueType === "RANKED_TFT_DOUBLE_UP"){
+            return (
+              <Text
+                key={i}
+                style={{ fontSize: 20, color: "red", fontWeight: "bold" }}
+              >
+                롤체: {content.tier} {content.rank} 승: {content.wins} 패:{" "}
+                {content.losses}
+              </Text>
+            );
+          } else {
+            return (
+              <Text
+                key={i}
+                style={{ fontSize: 20, color: "red", fontWeight: "bold" }}
+              >
+                {content.queueType}: {content.tier} {content.rank} 승: {content.wins} 패:{" "}
+                {content.losses}
+              </Text>
+            );
+          }
+        })}
+        
+      
+      </View>
+      <View>
+        <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => {
+              addFollow();
+            }}
+          >
+        
+            <Text>팔로우하기</Text>
+          </TouchableOpacity>
+      </View>
+      <View style={{ marginBottom: 30 }}>
+        {followNames.map((content, i) => {
+          return (
+            <Follow
+              content={content}
+              key={i}
+              ID={userId}
+              followNames={followNames}
+              setFollowNames={setFollowNames}
+            />
+          );
+        })}
+      </View>
     </ScrollView>
   );
 }
@@ -131,12 +233,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 20,
-    marginTop: 5,
+    marginTop: 1,
   },
   result: {
     fontSize: 20,
-    marginTop: 20,
+    marginVertical:0,
     color: "black",
     fontWeight: "bold",
+    
   },
 });
--- pages/MainPage.js
+++ pages/MainPage.js
@@ -11,6 +11,8 @@ import * as Application from "expo-application";
 import { firebase_db } from "../firebaseConfig";
 import Match from "../components/Match";
 import { SafeAreaView } from "react-native-safe-area-context";
+import axios from "axios";
+
 // import data from "../data.json";
 
 import {
@@ -27,16 +29,76 @@ import {
 
 export default function MainPage({ navigation }) {
   const userId = Application.androidId;
-
   const [totalData, setTotalData] = useState([]);
+  
+  const [fav_list, setfav_list] = useState([]);
+  const [fav_id, setfav_id] = useState([]);
 
-  const timestampDate = (timestamp) => {
-    let date = new Date(timestamp);
-  };
+  let riotApiKey = "RGAPI-1de6a553-746c-49bc-ab40-546c6d6ed8a9";
+
+
 
   const clearAll = () => {
     firebase_db.ref().remove()
   }
+
+  const loadMatchCode = (puuid,k) => {
+    axios.get("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids" +
+    "?api_key=" +
+    riotApiKey +
+    "&start=0&count=3" ).then((response) => {
+      let loadData = Object.values(response.data)
+
+      loadData.map((content)=>{
+        firebase_db.ref().child("userData").child(userId).child(content).set(k)
+      })
+
+    })
+  }
+
+  const getFav = () => {
+    firebase_db.ref("userData/"+userId).remove() // 초기화
+
+    firebase_db
+   .ref("users/" + userId)
+   .once("value")
+   .then((snapshot) => {
+   let fav = snapshot.val();
+   setfav_list(Object.keys(fav))
+   setfav_id(Object.values(fav))
+   })
+
+   
+   fav_id.map((content,i)=>{
+     loadMatchCode(content,fav_list[i])
+     
+   })
+   
+   if (fav_list && fav_list.length > 0) {
+     console.log('매치코드 추가 끝');
+   } else {
+     console.log("불러오기실패");
+   }
+  };
+
+
+  let getMatchDataUrl = 'https://asia.api.riotgames.com/lol/match/v5/matches/'
+  const AboutMatch = (matchId) => {
+    axios.get(getMatchDataUrl+matchId+"?api_key=" + riotApiKey).then((response)=>{
+      let data = response.data
+      firebase_db.ref().child("userMatchData").child(userId).child(matchId).set(data)
+    });
+  };
+
+  const MatchDataLoading = () => {
+    firebase_db.ref("userData/" + userId).once("value").then((snapshot) => {
+      let matchList = Object.keys(snapshot.val())
+      matchList.map((matchCode)=> {
+        firebase_db.ref().child("userMatchData").child(userId).set(null)
+        AboutMatch(matchCode)
+      })
+    })
+  };
 
   const dataReady = () => {
     firebase_db
@@ -49,6 +111,7 @@ export default function MainPage({ navigation }) {
         console.log("데이터준비완료 !!!!");
       });
   };
+
 
   useEffect(() => {
     navigation.setOptions({
@@ -83,21 +146,26 @@ export default function MainPage({ navigation }) {
           <TouchableOpacity
             style={styles.myButton}
             onPress={() => {
-              navigation.navigate("FollowPage",{ID: userId});
+              navigation.navigate("FollowPage",{ID: userId, riotApiKey: riotApiKey});
             }}
           >
             <Text style={styles.myPageButtonText}>팔로우</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.myButton}
-            onPress={() => {
-              dataReady();
+            onPress={async () => {
+              await getFav();
+              await MatchDataLoading();
+              await dataReady();
             }}
           >
-            <Text style={styles.myPageButtonText}>검색</Text>
+            <Text style={styles.myPageButtonText}>전적검색</Text>
           </TouchableOpacity>
         </View>
         <View style={{ alignItems: "center", marginVertical: 20 }}>
+          <TouchableOpacity onPress={()=>{getFav()}}><Text>get Fav</Text></TouchableOpacity>
+          <TouchableOpacity onPress={()=>{MatchDataLoading()}}><Text>MatchDataLoading</Text></TouchableOpacity>
+          <TouchableOpacity onPress={()=>{dataReady()}}><Text>dataReady</Text></TouchableOpacity>
           <BannerAd
             unitId={TestIds.BANNER}
             size={BannerAdSize.LARGE_BANNER}
